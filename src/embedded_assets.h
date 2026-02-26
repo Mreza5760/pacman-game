14cd9042c34fb7768a414aa6f85f05763fab7155
Mreza5760 <m.reza.576084@gmail.com>
2026-02-26 22:38:40 +0330

    add exe

---
+++ src/embedded_assets.h
@@ -0,0 +1,313 @@
+#ifndef EMBEDDED_ASSETS_H
+#define EMBEDDED_ASSETS_H
+
+extern unsigned char icon_ico[];
+extern unsigned int icon_ico_size;
+
+extern unsigned char icon_png[];
+extern unsigned int icon_png_size;
+
+extern unsigned char rank_txt[];
+extern unsigned int rank_txt_size;
+
+extern unsigned char setbackt_ttf[];
+extern unsigned int setbackt_ttf_size;
+
+extern unsigned char gameover_png[];
+extern unsigned int gameover_png_size;
+
+extern unsigned char gamewin_png[];
+extern unsigned int gamewin_png_size;
+
+extern unsigned char pacmantext_png[];
+extern unsigned int pacmantext_png_size;
+
+extern unsigned char apple_png[];
+extern unsigned int apple_png_size;
+
+extern unsigned char cherry_png[];
+extern unsigned int cherry_png_size;
+
+extern unsigned char heart_png[];
+extern unsigned int heart_png_size;
+
+extern unsigned char mushroom_png[];
+extern unsigned int mushroom_png_size;
+
+extern unsigned char pepper_png[];
+extern unsigned int pepper_png_size;
+
+extern unsigned char s0_mp3[];
+extern unsigned int s0_mp3_size;
+
+extern unsigned char s1_mp3[];
+extern unsigned int s1_mp3_size;
+
+extern unsigned char s2_mp3[];
+extern unsigned int s2_mp3_size;
+
+extern unsigned char s3_mp3[];
+extern unsigned int s3_mp3_size;
+
+extern unsigned char berrypie0_png[];
+extern unsigned int berrypie0_png_size;
+
+extern unsigned char berrypie1_png[];
+extern unsigned int berrypie1_png_size;
+
+extern unsigned char berrypie2_png[];
+extern unsigned int berrypie2_png_size;
+
+extern unsigned char berrypie3_png[];
+extern unsigned int berrypie3_png_size;
+
+extern unsigned char berrypie4_png[];
+extern unsigned int berrypie4_png_size;
+
+extern unsigned char berrypie5_png[];
+extern unsigned int berrypie5_png_size;
+
+extern unsigned char berrypie6_png[];
+extern unsigned int berrypie6_png_size;
+
+extern unsigned char berrypie7_png[];
+extern unsigned int berrypie7_png_size;
+
+extern unsigned char berrypie8_png[];
+extern unsigned int berrypie8_png_size;
+
+extern unsigned char blinky0_png[];
+extern unsigned int blinky0_png_size;
+
+extern unsigned char blinky1_png[];
+extern unsigned int blinky1_png_size;
+
+extern unsigned char blinky2_png[];
+extern unsigned int blinky2_png_size;
+
+extern unsigned char blinky3_png[];
+extern unsigned int blinky3_png_size;
+
+extern unsigned char blinky4_png[];
+extern unsigned int blinky4_png_size;
+
+extern unsigned char blinky5_png[];
+extern unsigned int blinky5_png_size;
+
+extern unsigned char blinky6_png[];
+extern unsigned int blinky6_png_size;
+
+extern unsigned char blinky7_png[];
+extern unsigned int blinky7_png_size;
+
+extern unsigned char blinky8_png[];
+extern unsigned int blinky8_png_size;
+
+extern unsigned char blue0_png[];
+extern unsigned int blue0_png_size;
+
+extern unsigned char blue1_png[];
+extern unsigned int blue1_png_size;
+
+extern unsigned char blue2_png[];
+extern unsigned int blue2_png_size;
+
+extern unsigned char blue3_png[];
+extern unsigned int blue3_png_size;
+
+extern unsigned char clyde0_png[];
+extern unsigned int clyde0_png_size;
+
+extern unsigned char clyde1_png[];
+extern unsigned int clyde1_png_size;
+
+extern unsigned char clyde2_png[];
+extern unsigned int clyde2_png_size;
+
+extern unsigned char clyde3_png[];
+extern unsigned int clyde3_png_size;
+
+extern unsigned char clyde4_png[];
+extern unsigned int clyde4_png_size;
+
+extern unsigned char clyde5_png[];
+extern unsigned int clyde5_png_size;
+
+extern unsigned char clyde6_png[];
+extern unsigned int clyde6_png_size;
+
+extern unsigned char clyde7_png[];
+extern unsigned int clyde7_png_size;
+
+extern unsigned char clyde8_png[];
+extern unsigned int clyde8_png_size;
+
+extern unsigned char eye0_png[];
+extern unsigned int eye0_png_size;
+
+extern unsigned char eye1_png[];
+extern unsigned int eye1_png_size;
+
+extern unsigned char eye2_png[];
+extern unsigned int eye2_png_size;
+
+extern unsigned char eye3_png[];
+extern unsigned int eye3_png_size;
+
+extern unsigned char inky0_png[];
+extern unsigned int inky0_png_size;
+
+extern unsigned char inky1_png[];
+extern unsigned int inky1_png_size;
+
+extern unsigned char inky2_png[];
+extern unsigned int inky2_png_size;
+
+extern unsigned char inky3_png[];
+extern unsigned int inky3_png_size;
+
+extern unsigned char inky4_png[];
+extern unsigned int inky4_png_size;
+
+extern unsigned char inky5_png[];
+extern unsigned int inky5_png_size;
+
+extern unsigned char inky6_png[];
+extern unsigned int inky6_png_size;
+
+extern unsigned char inky7_png[];
+extern unsigned int inky7_png_size;
+
+extern unsigned char inky8_png[];
+extern unsigned int inky8_png_size;
+
+extern unsigned char pinky0_png[];
+extern unsigned int pinky0_png_size;
+
+extern unsigned char pinky1_png[];
+extern unsigned int pinky1_png_size;
+
+extern unsigned char pinky2_png[];
+extern unsigned int pinky2_png_size;
+
+extern unsigned char pinky3_png[];
+extern unsigned int pinky3_png_size;
+
+extern unsigned char pinky4_png[];
+extern unsigned int pinky4_png_size;
+
+extern unsigned char pinky5_png[];
+extern unsigned int pinky5_png_size;
+
+extern unsigned char pinky6_png[];
+extern unsigned int pinky6_png_size;
+
+extern unsigned char pinky7_png[];
+extern unsigned int pinky7_png_size;
+
+extern unsigned char pinky8_png[];
+extern unsigned int pinky8_png_size;
+
+extern unsigned char rocky0_png[];
+extern unsigned int rocky0_png_size;
+
+extern unsigned char rocky1_png[];
+extern unsigned int rocky1_png_size;
+
+extern unsigned char rocky2_png[];
+extern unsigned int rocky2_png_size;
+
+extern unsigned char rocky3_png[];
+extern unsigned int rocky3_png_size;
+
+extern unsigned char rocky4_png[];
+extern unsigned int rocky4_png_size;
+
+extern unsigned char rocky5_png[];
+extern unsigned int rocky5_png_size;
+
+extern unsigned char rocky6_png[];
+extern unsigned int rocky6_png_size;
+
+extern unsigned char rocky7_png[];
+extern unsigned int rocky7_png_size;
+
+extern unsigned char rocky8_png[];
+extern unsigned int rocky8_png_size;
+
+extern unsigned char snowwhite0_png[];
+extern unsigned int snowwhite0_png_size;
+
+extern unsigned char snowwhite1_png[];
+extern unsigned int snowwhite1_png_size;
+
+extern unsigned char snowwhite2_png[];
+extern unsigned int snowwhite2_png_size;
+
+extern unsigned char snowwhite3_png[];
+extern unsigned int snowwhite3_png_size;
+
+extern unsigned char snowwhite4_png[];
+extern unsigned int snowwhite4_png_size;
+
+extern unsigned char snowwhite5_png[];
+extern unsigned int snowwhite5_png_size;
+
+extern unsigned char snowwhite6_png[];
+extern unsigned int snowwhite6_png_size;
+
+extern unsigned char snowwhite7_png[];
+extern unsigned int snowwhite7_png_size;
+
+extern unsigned char snowwhite8_png[];
+extern unsigned int snowwhite8_png_size;
+
+extern unsigned char pacclosed_png[];
+extern unsigned int pacclosed_png_size;
+
+extern unsigned char pacnarrow_png[];
+extern unsigned int pacnarrow_png_size;
+
+extern unsigned char pacnarrow2_png[];
+extern unsigned int pacnarrow2_png_size;
+
+extern unsigned char pacwide_png[];
+extern unsigned int pacwide_png_size;
+
+extern unsigned char pacwide2_png[];
+extern unsigned int pacwide2_png_size;
+
+extern unsigned char death1_png[];
+extern unsigned int death1_png_size;
+
+extern unsigned char death10_png[];
+extern unsigned int death10_png_size;
+
+extern unsigned char death11_png[];
+extern unsigned int death11_png_size;
+
+extern unsigned char death2_png[];
+extern unsigned int death2_png_size;
+
+extern unsigned char death3_png[];
+extern unsigned int death3_png_size;
+
+extern unsigned char death4_png[];
+extern unsigned int death4_png_size;
+
+extern unsigned char death5_png[];
+extern unsigned int death5_png_size;
+
+extern unsigned char death6_png[];
+extern unsigned int death6_png_size;
+
+extern unsigned char death7_png[];
+extern unsigned int death7_png_size;
+
+extern unsigned char death8_png[];
+extern unsigned int death8_png_size;
+
+extern unsigned char death9_png[];
+extern unsigned int death9_png_size;
+
+#endif // EMBEDDED_ASSETS_H
